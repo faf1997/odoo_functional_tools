abbb3194d945b3905bf2ea4c3513e5d1936a3dc8
faf1997 <alejandrofranfio@gmail.com>
2025-05-14 12:59:15 -0300

    [ADD] visible_sale_order_date

---
+++ visible_sale_order_date/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Visible sale order date',
+    'version': '17.0.1.0.0',
+    'category': 'Technical Settings',
+    'depends': ['sale'],
+    'author': 'Francisco Fiorentino',
+    'license': 'LGPL-3',
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
