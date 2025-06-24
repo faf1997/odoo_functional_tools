d83ed5c113c9cc88f9f69c275f93d6c794c51657
faf1997 <115713214+faf1997@users.noreply.github.com>
2025-06-24 12:34:17 -0300

    Update res_partner.py

--- partner_code/models/res_partner.py
+++ partner_code/models/res_partner.py
@@ -12,18 +12,3 @@ class ResPartner(models.Model):
         ('customer_code_unique', 'unique(customer_code)', 'The customer code must be unique.'),
     ]
 
-    @api.model
-    def create(self, vals):
-        if not vals.get('customer_code'):
-            vals['customer_code'] = self.env['ir.sequence'].next_by_code('res.partner.customer.code')
-        return super().create(vals)
-
-
-    def write(self, vals):
-        if 'customer_code' in vals:
-            for partner in self.filtered(lambda p: p.customer_code and vals.get('customer_code') and p.customer_code != vals.get('customer_code')):
-                raise UserError(_('You cannot modify an existing customer code.'))
-            # remove any accidental change
-            vals.pop('customer_code', None)
-        return super().write(vals)
-
