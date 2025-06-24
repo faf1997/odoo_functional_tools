ce07fa4aca05508eb5ec31b6cbb1dc46c1c5f4f6
faf1997 <alejandrofranfio@gmail.com>
2025-06-24 11:19:06 -0300

    [UPD]

--- partner_code/__init__.py
+++ partner_code/__init__.py
@@ -1,9 +1,11 @@
 from . import models 
-from odoo import api, SUPERUSER_ID
 
-def assign_existing_customer_codes(cr, registry):
-env = api.Environment(cr, SUPERUSER_ID, {})
-partners = env['res.partner'].search([('customer_code', '=', False)])
-for partner in partners:
-code = env['ir.sequence'].next_by_code('res.partner.customer.code')
-partner.sudo().write({'customer_code': code})
+
+def assign_existing_customer_codes(env):
+    """
+    Assigns customer_code to existing partners without one.
+    """
+    partners = env['res.partner'].search([('customer_code', '=', False)])
+    for partner in partners:
+        code = env['ir.sequence'].next_by_code('res.partner.customer.code')
+        partner.sudo().write({'customer_code': code})
