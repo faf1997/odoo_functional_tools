164c011181b6405ef7268d30205f760d5f192f38
faf1997 <alejandrofranfio@gmail.com>
2025-07-13 18:47:59 -0300

    [ADD] fields_module

---
+++ fields_module/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Model Fields Module Name',
+    'version': '17.0.1.0.0',
+    'category': 'Technical Settings',
+    'depends': ['base'],
+    'author': 'Francisco Fiorentino',
+    'license': 'LGPL-3',
+    'data': [
+        'views/ir_model_fields_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
