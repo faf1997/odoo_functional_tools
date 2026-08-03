2122c6ac8bbad4ed67d3ef6a699bef4f01a81127
faf1997 <alejandrofranfio@gmail.com>
2026-08-03 14:40:59 -0300

    [ADD] account_move_analysis: vistas graph y pivot para account.move

---
+++ account_move_analysis/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Análisis de facturación",
+    "version": "17.0.1.0.0",
+    "category": "Accounting",
+    "depends": ["account"],
+    "author": "Francisco Fiorentino",
+    "license": "LGPL-3",
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
